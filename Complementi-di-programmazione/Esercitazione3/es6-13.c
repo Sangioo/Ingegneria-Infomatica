3e4aa993d12fcc9fda7a0455dce58cb75306ac5e
Sangio <sangio@DESKTOP-G08FJ5E>
2024-03-16 13:01:49 +0100

    finita l'esercitazione 3 di complementi di programmazione

--- Complementi-di-programmazione/Esercitazione3/es6-13.c
+++ Complementi-di-programmazione/Esercitazione3/es6-13.c
@@ -18,7 +18,7 @@ int* allocaInt() {
 // void printInt(int *i1, int *i2);
 // che, dati in input due puntatori ad intero ne stampa i valori.
 void printInt(int *i1, int *i2) {
-	printf("i1: %d\ti2: %d\n", *i1, *i2);
+	printf("i1_7: %d\ti2_7: %d\n", *i1, *i2);
 	return;
 }
 
@@ -62,49 +62,54 @@ double* soluzioneEquazionePtr(int a, int b, int c) {
 }
 void* soluzioneEquazionePtrComplessi(int a, int b, int c) {
 	void *result;
-	double delta = b*b - 4*a*c;
+	char *campo;
+	double delta = b*b - 4*a*c, *solve;
 
 	if (delta < 0) {
+		result = malloc(sizeof(char) + sizeof(double) * 2);
+		campo = (char *)result;
+		*campo = 'C';
+		solve = (double *)(campo + 1);
 
-
-		return NULL;
+		double modulo = fabs(delta);
+		*solve = -b / 2*a;
+		*(solve + 1) = sqrt(modulo) / 2*a;
 	} else if (fabs(delta - 0.0) < 1e-6) {
 		result = malloc(sizeof(char) + sizeof(double));
-		char *c = (char *)result;
-		*c = 'R';
-		double *resolve = (double *) (c + 1);
+		campo = (char *)result;
+		*campo = 'R';
+		solve = (double *) (campo + 1);
 
-		*resolve = (-b)/2*a;
-
-		return result;
+		*solve = -b / 2*a;
 	} else {
 		result = malloc(sizeof(char) + sizeof(double) * 2);
-		char *c = (char *)result;
-		*c = 'R';
-		double *resolve = (double *) (c + 1);
+		campo = (char *)result;
+		*campo = 'R';
+		solve = (double *) (campo + 1);
 
-		*resolve = (-b + sqrt(delta))/2*a;
-		*(resolve + 1) = (-b - sqrt(delta))/2*a;
-
-		return result;
+		*solve = (-b + sqrt(delta)) / 2*a;
+		*(solve + 1) = (-b - sqrt(delta)) / 2*a;
 	}
+
+	return result;
 }
 
 // ES 8b
 // Scrivere la funzione
 // void printSoluzione(void *soluzione);
 // che, dato in ingresso l’output della funzione precedente ne stampi il risultato.
-// void printSoluzione(void *soluzione) {
-// 	char *c = (char *)soluzione;
-// 	double *s = (double *)(c + 1);
-
-// 	int i = 0;
-// 	while ((s + 1) != NULL) {
-// 		printf("ris %d: %lf", i,  *(s + i));
-// 		i++;
-// 	}
-// 	return;
-// }
+void printSoluzione(void *soluzione) {
+	char *campo = (char *)soluzione;
+	double *zeri = (double *)(campo + 1);
+	if (*campo == 'R') {
+		printf("soluzione nel campo reale\n");
+		printf("zeri della funzione: %lf %lf\n", *zeri, *(zeri + 1));
+	} else {
+		printf("soluzione nel campo complesso\n");
+		printf("zeri della funzione: %lf ±%lfi\n", *zeri, *(zeri + 1));
+	}
+	return;
+}
 
 // ES 9
 // Scrivere la funzione
@@ -137,30 +142,21 @@ int mcm(int i1, int i2) {
 // ES 11
 // Scrivere la funzione
 // void conversioneTemperatura(float t, char c);
-// Che prende in input una temperatura t e una scala di riferimento data dal char c. La funzione deve effettuare la conversione nelle altre scale, esattamente come nell’esercizio 3.3.
+// Che prende in input una temperatura t e una scala di riferimento data dal char c. La funzione deve effettuare la conversione nelle altre scale, esattamente come nell’***** esercizio 3.3 *****.
 // Variante: scrivere una funzione che restituisca i risultati come parametri di output.
 void conversioneTemperatura(float t, char c, float* resK, float* resC, float* resF) {
 	if (c == 'C') {
 		*resC = t;
 		*resK = t + 273.15f;
 		*resF = (t * 1.8f) + 32;
-		printf("temperatura in Celsius: %lf\n", t);
-		printf("temperatura in Kelvin: %lf\n", t + 273.15f);
-		printf("temperatura in Fahrenheit: %lf\n", (t * 1.8f) + 32);
 	} else if (c == 'K') {
 		*resC = t - 273.15f;
 		*resK = t;
 		*resF = ((t - 273.15f) * 1.8f) + 32;
-		printf("temperatura in Celsius: %lf\n", t - 273.15f);
-		printf("temperatura in Kelvin: %lf\n", t);
-		printf("temperatura in Fahrenheit: %lf\n", ((t - 273.15f) * 1.8f) + 32);
 	} else if (c == 'F') {
 		*resC = (t - 32) * 5/9;
 		*resK = (t - 32) * 5/9 + 273.15f;
 		*resF = t;
-		printf("temperatura in Celsius: %lf\n", (t - 32) * 5/9);
-		printf("temperatura in Kelvin: %lf\n", (t - 32) * 5/9 + 273.15f);
-		printf("temperatura in Fahrenheit: %lf\n", t);
 	}
 	return;
 }
@@ -172,73 +168,142 @@ void conversioneTemperatura(float t, char c, float* resK, float* resC, float* re
 // Inoltre, deve ritornare il puntatore alla memoria allocata contenente la soluzione (il risultato della conversione in entrambe le scale).
 // Le due conversioni risultanti devono essere salvate in una zona di memoria allocata dinamicamente con un'unica chiamata alla funzione malloc().
 void* conversioneTemperaturaP(float *t, char c) {
-	void* res = malloc(sizeof(float) * 3);
-	float *resC = (float *)res;
-	float *resK = (float *)(resC + 1);
-	float *resF = (float *)(resC + 2);
+	void* res = malloc(sizeof(float) * 2 + sizeof(char) * 2);
+	char *res1c = (char *)res;
+	float *res1 = (float *)(res1c + 1);
+	char *res2c = (char *)(res1 + 1);
+	float *res2 = (float *)(res2c + 1);
+
 	if (c == 'C') {
-		*resC = *t;
-		*resK = *t + 273.15f;
-		*resF = (*t * 1.8f) + 32;
+		*res1c = 'K';
+		*res1 = *t + 273.15f;
+		*res2c = 'F';
+		*res2 = (*t * 1.8f) + 32;
 	} else if (c == 'K') {
-		*resC = *t - 273.15f;
-		*resK = *t;
-		*resF = ((*t - 273.15f) * 1.8f) + 32;
+		*res1c = 'C';
+		*res1 = *t - 273.15f;
+		*res2c = 'F';
+		*res2 = ((*t - 273.15f) * 1.8f) + 32;
 	} else if (c == 'F') {
-		*resC = (*t - 32) * 5/9;
-		*resK = (*t - 32) * 5/9 + 273.15f;
-		*resF = *t;
+		*res1c = 'C';
+		*res1 = (*t - 32) * 5/9;
+		*res2c = 'K';
+		*res2 = (*t - 32) * 5/9 + 273.15f;
 	}
 	return res;
 }
 
+// ES 13
+// Scrivere la funzione
+// void printConversione(void *conversioni);
+// che, dato in ingresso l’output della funzione precedente ne stampi il risultato.
+// N.B. nella zona di memoria conversioni sono memorizzati in maniera contigua sia il valore numerico che la scala di entrambe le conversioni.
+void printConversione(void *conversioni) {
+	char *c1 = (char *)conversioni;
+	float *t1 = (float *)(c1 + 1);
+	char *c2 = (char *)(t1 + 1);
+	float *t2 = (float *)(c2 + 1);
+
+	printf("temperatura in %c: %f\n", *c1, *t1);
+	printf("tempeartura in %c: %f\n", *c2, *t2);
+
+	return;
+}
+
 int main() {
 	// ES 6
-	int *p1;
-	p1 = allocaInt();
-	printf("p1: %x\n", p1);
+	printf("***** ESERCIZIO 6:  *****\n");
+	int *p1_6;
+	p1_6 = allocaInt();
+	printf("p1_6: %x\n", p1_6);
+
 
 	// ES 7
-	int i1 = 10, i2 = 20;
-	printInt(&i1, &i2);
+	printf("\n\n***** ESERCIZIO 7:  *****\n");
+	int i1_7 = 10, i2_7 = 20;
+	printInt(&i1_7, &i2_7);
+
 
 	// ES 8
-	int a = 1, b = 2, c = 0;
-	double x1, x2, *s2;
-	bool f;
-	void *s3;
+	printf("\n\n***** ESERCIZIO 8:  *****\n");
+	int a_8 = 1, b_8 = 2, c_8 = 3;
+	double x1_8, x2_8, *sol2_8, *zeri_8;
+	bool sol1_8;
+	void *sol3_8;
+	char *campoSol3_8;
 
-	f = soluzioneEquazione(a, b, c, &x1, &x2);
-	if (f) {
-		printf("zeri della funzione: %lf %lf\n", x1, x2);
+	sol1_8 = soluzioneEquazione(a_8, b_8, c_8, &x1_8, &x2_8);
+	if (sol1_8) {
+		printf("1) zeri della funzione: %lf %lf\n", x1_8, x2_8);
 	} else {
-		printf("la funzione non tocca l'asse x\n");
+		printf("1) la funzione non tocca l'asse x\n");
 	}
 
-	s2 = soluzioneEquazionePtr(a, b, c);
-	if (s2) {
-		printf("zeri della funzione: %lf %lf\n", *s2, *(s2 + 1));
+	sol2_8 = soluzioneEquazionePtr(a_8, b_8, c_8);
+	if (sol2_8) {
+		printf("2) zeri della funzione: %lf %lf\n", *sol2_8, *(sol2_8 + 1));
 	} else {
-		printf("la funzione non tocca l'asse x\n");
+		printf("2) la funzione non tocca l'asse x\n");
 	}
 
-	s3 = soluzioneEquazionePtrComplessi(a, b, c);
+	sol3_8 = soluzioneEquazionePtrComplessi(a_8, b_8, c_8);
+	campoSol3_8 = (char *)sol3_8;
+	zeri_8 = (double *)(campoSol3_8 + 1);
+	if (*campoSol3_8 == 'R') {
+		printf("3) soluzione nel campo reale\n");
+		printf("   zeri della funzione: %lf %lf\n", *zeri_8, *(zeri_8 + 1));
+	} else {
+		printf("3) soluzione nel campo complesso\n");
+		printf("   zeri della funzione: %lf ±%lfi\n", *zeri_8, *(zeri_8 + 1));
+	}
+
 
 	// ES 8b
-	// printSoluzione(s3);
+	printf("\n\n***** ESERCIZIO 8b: *****\n");
+	printSoluzione(sol3_8);
+
 
 	// ES 9
-	int i19 = 7, i29 = 24;
-	int mcd = MCD(i19, i29);
-	printf("mcd: %d\n", mcd);
+	printf("\n\n***** ESERCIZIO 9:  *****\n");
+	int i1_9 = 7, i2_9 = 24;
+	int mcd = MCD(i1_9, i2_9);
+
+	printf("i1_9: %d \ti2_9: %d\n", i1_9, i2_9);
+	printf("MCD: %d\n", mcd);
 
 	// ES 10
-	int i10 = 7, i20 = 24;
-	int m = mcm(i10, i20);
-	printf("mcd: %d\n", m);
+	printf("\n\n***** ESERCIZIO 10: *****\n");
+	int i1_10 = 7, i2_10 = 24;
+	int m = mcm(i1_10, i2_10);
+
+	printf("i1_10: %d\ti2_10: %d\n", i1_10, i2_10);
+	printf("mcm: %d\n", m);
+
 
 	// ES 11
+	printf("\n\n***** ESERCIZIO 11: *****\n");
+	float temp_11 = 20, resK_11, resC_11, resF_11;
+	char scala_11 = 'F';
 
+	printf("temperatura input in %c: %f\n\n", scala_11, temp_11);
+	conversioneTemperatura(temp_11, scala_11, &resK_11, &resC_11, &resF_11);
+	printf("temperatura in K: %f\n", resK_11);
+	printf("temperatura in C: %f\n", resC_11);
+	printf("temperatura in F: %f\n", resF_11);
+
+
+	// ES 12
+	printf("\n\n***** ESERCIZIO 12: *****\n");
+	float temp_12 = 12;
+	char scala_12 = 'C';
+
+	printf("temperatura in %c: %f\n", scala_12, temp_12);
+	void *conversioni_12 = conversioneTemperaturaP(&temp_12, scala_12);
+
+
+	// ES 13
+	printf("\n\n***** ESERCIZIO 13: *****\n");
+	printConversione(conversioni_12);
 
 
 	return 0;
